d600356854503d62e9400037de657402cb77bba1
Devil Dad <85053195+kkverma25@users.noreply.github.com>
2021-12-22 23:11:27 +0530

    Update upload.py

--- helpers/upload.py
+++ helpers/upload.py
@@ -76,7 +76,7 @@ async def upload_subtitle(client, message, file_loc):
         await client.send_document(
             chat_id=message.chat.id,
             document=file_loc,
-            caption="**@TroJanzHEX**",
+            caption="**@D_E_V_l_L**",
             progress=progress_func,
             progress_args=(
                 "**Uploading extracted subtitle...**",
